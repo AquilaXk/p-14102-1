6fccf8f8075bc82ed6d0a680175ed5b075e94e2a
AquilaXk <illusiveman7@gmail.com>
2025-12-03 12:22:51 +0900

    35강 완료

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,10 +1,19 @@
 package com.back.domain.post.post.entity;
 
 
+import com.back.domain.post.postComment.entity.PostComment;
 import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.PERSIST;
+import static jakarta.persistence.CascadeType.REMOVE;
+import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Getter
@@ -12,6 +21,9 @@ import lombok.NoArgsConstructor;
 public class Post extends BaseEntity {
     private String title;
     private String content;
+
+    @OneToMany(mappedBy="post", fetch = LAZY, cascade = {PERSIST, REMOVE})
+    private List<PostComment> comments = new ArrayList<>();
 
     public Post(String title, String content) {
         this.title = title;
@@ -21,6 +33,12 @@ public class Post extends BaseEntity {
     public void modify(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+    // this 는 Post 객체 전달
+    public PostComment addComment(String content) {
+        PostComment postComment = new PostComment(this, content);
+        comments.add(postComment);
+        return postComment;
     }
 
 }
